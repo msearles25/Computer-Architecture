7bb93abafbf0e168409efd4833142e252bec7ce9
Michael <skaman4459@gmail.com>
2020-05-20 11:06:50 -0400

    day two

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,19 +17,30 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(sys.argv[1]) as f:
+            for line in f:
+                string_val = line.split("#")[0].strip()
+                if string_val == '':
+                    continue
+                v = int(string_val, 2)
+                #print(v)
+                self.ram[address] = v
+                address += 1
+
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -38,14 +49,16 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
     def ram_read(self, mar):
-        return self.ram[mar] 
+        return self.reg[mar] 
 
     def ram_write(self, mar, mdr): 
-        self.ram[mar] = mdr
+        self.reg[mar] = mdr
 
     def trace(self):
         """
@@ -83,11 +96,19 @@ class CPU:
                 mar = self.ram[self.pc + 1]
                 value = self.ram_read(mar)
                 
-                print('The value ', value)
+                print(value)
                 
                 self.pc += 2
+            elif instruction == 0b10100010:
+                
+                reg_a = self.ram[self.pc+1]
+                reg_b = self.ram[self.pc+2]
+                value = self.alu("MUL", reg_a, reg_b)
+
+                self.pc += 3
+
             elif instruction == 0b00000001:
                 halted = True
             else:
-                print(f'unknown instruction {instruction} at address {pc}')
+                print(f'unknown instruction {instruction} at address {self.pc}')
                 sys.exit(1)
